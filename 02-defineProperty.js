a994d2a025daf2081750c0497099cab23836a9df
BrentonPhoenix <brentonphoenix@gmail.com>
2023-01-08 14:50:39 -0500

    further testing reason: curiosity

--- 02-defineProperty.js
+++ 02-defineProperty.js
@@ -7,6 +7,14 @@ Object.defineProperty(objectOne, 'property1', {
 
 objectOne.property1 =77
 
-console.log(objectOne.property1)
+console.log("objectOne" , objectOne.property1)
+
+
 
 //writable:false makes it readonly
+
+console.log("objectOne:" , objectOne)
+// console.log("objectOne:" , ...objectOne)
+console.log(Object.getOwnPropertyNames(objectOne))
+
+//looks like you have to use getOwnPropertyNames on the object if you want to get the property names. I thought that destructuring it might work but it throws an error (TypeError: Found non-callable @@iterator)
